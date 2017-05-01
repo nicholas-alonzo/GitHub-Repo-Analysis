26325ff61e5d5c8942894fab954e37bc867dfa4e
nicholas-alonzo <nalonzo@ucdavis.edu>
2017-05-01 16:37:06 -0700

    new function in module

--- gharch.py
+++ gharch.py
@@ -1,5 +1,7 @@
 import os
 import requests
+import requests_cache
+import lxml.html as lx
 import wget
 import pandas as pd
 import json
@@ -52,6 +54,7 @@ def download_events(files_to, ymd, to_endDay=None):
                 except:
                     print "Could not download %s" % url
 
+
 # unzips the downloaded files from download_events()
 def unzip_files(files_path, command=None):
     """
@@ -71,6 +74,8 @@ def unzip_files(files_path, command=None):
 
     os.chdir(cwd)
 
+
+# make pandas df from json file
 def make_df(file):
     """
     :param file: 
@@ -83,6 +88,75 @@ def make_df(file):
 
     return pd.DataFrame(data)
 
+
+# find root repositories created
+def find_rootrepos(df, cache='cache'):
+    """
+    :param df: pandas DF
+    :param cache: name of cache
+    :return: a subset of df with the following conditions applied
+            - not from an organization
+            - are repos
+            - are not github.io projects
+            - exists to date
+            - have at least 3 contributors to date
+    """
+    # not from an organization
+    cond1 = df['type'] == 'CreateEvent'
+    cond2 = pd.isnull(df['org'])
+
+    df_sub = df[cond1 & cond2]
+    df_sub = df_sub.reset_index(drop=True)
+
+    # are repos
+    df_sub_payload = df_sub['payload'].tolist()
+    df_sub_payload = pd.DataFrame(df_sub_payload)
+
+    ghpl = df_sub_payload[pd.isnull(df_sub_payload['ref'])]
+    keep_rows = ghpl.index.values
+
+    df_sub = df_sub.iloc[keep_rows]
+    df_sub = df_sub.reset_index(drop=True)
+
+    # are not github.io OR
+    df_sub_repos = df_sub['repo'].tolist()
+    df_sub_repos = pd.DataFrame(df_sub_repos)
+
+    ghio = df_sub_repos[~df_sub_repos['name'].str.contains('.github.io|github.com')]
+    keep_rows = ghio.index.values
+
+    df_sub = df_sub.iloc[keep_rows]
+    df_sub = df_sub.reset_index(drop=True)
+
+    # exists
+    # have at least 3 contributors to date
+    df_sub_repos = df_sub['repo'].tolist()
+    df_sub_repos = pd.DataFrame(df_sub_repos)
+
+    repo_names = df_sub_repos['name'].tolist()
+
+    L = []
+    indicies = 0
+
+    for name in repo_names[:50]:
+
+        requests_cache.install_cache(cache)
+        url = 'https://github.com/%s/contributors_size' % name
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            html = lx.fromstring(response.text)
+            contr_count = html.xpath("number(//span/text())")
+            if contr_count > 3:
+                L.append(indicies)
+            else:
+                pass
+        except:
+            pass
+
+        indicies += 1
+
+        return df_sub.iloc[L]
 
 # RESOURCES
 # https://stackoverflow.com/questions/7656754/place-a-0-in-front-of-numbers-in-a-list-if-they-are-less-than-ten-in-python
